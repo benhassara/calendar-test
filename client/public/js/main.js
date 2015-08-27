e96c7117ab1fbcfa390d23aad4899dec69b34880
benhassara <bhassara@gmail.com>
2015-08-27 14:21:21 -0600

    its all broken

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,34 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  $("#calendar").fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      defaultView: 'agendaWeek',
+      defaultDate: '2014-06-12',
+      editable: true,
+      lazyFetching: true
+    });
+
+
+  $('#add-event').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $('#event-modal').modal('show');
+    $('#event-modal').on('hide.bs.modal', clearForm);
+  });
 });
+
+
+function clearForm() {
+  $('#event-name').val('');
+  $('#event-desc').val('');
+  $('#event-date').val('');
+  $('#event-start-time').val('');
+  $('#event-end-time').val('');
+  $('#event-url').val('');
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Calendar' });
+});
+
+router.get('/form', function(req, res, next) {
+  res.render('form');
 });
 
 module.exports = router;
